cce31f90af4b4e2c6176a33294d9fac2ff7485dc
Tarun M Krishna <43349589+taruntmk2712@users.noreply.github.com>
2021-02-26 18:14:13 +0530

    Delete ShoppingDen directory

--- ShoppingDen/src/main/java/com/tata/shoppingden/dao/ShoppingCartDao.java
+++
@@ -1,9 +0,0 @@
-package com.tata.shoppingden.dao;
-
-import com.tata.shoppingden.models.ShoppingCart;
-
-import java.util.List;
-
-public interface ShoppingCartDao {
-    public List<ShoppingCart> displayCart(long uid) throws Exception;
-}
